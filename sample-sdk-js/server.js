a12b15bf41af65db45333991afd25a83d0595771
Khoi-Nguyen Nguyen-Hoang <khoi-nguyen.nguyen-hoang@ekino.com>
2024-09-19 10:29:57 +0700

    chore(demo): update demo

--- sample-sdk-js/server.js
+++ sample-sdk-js/server.js
@@ -6,8 +6,16 @@ const port = 8080;
 app.use("/src", express.static(path.join(__dirname, "src")));
 
 // Set up a route for ignore-index.html.
-app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/src/ignore-index.html'));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/src/index.html'));
+});
+
+app.get('/dashboard', function (req, res) {
+  res.sendFile(path.join(__dirname + '/src/dashboard.html'));
+});
+
+app.get('/expired-link', function (req, res) {
+  res.sendFile(path.join(__dirname + '/src/expired-link.html'));
 });
 
 // Start the server.
